1a54f5c59c664d17c5a02738e9bb34e947774f22
Matthew Marbury <mmarbury@openlending.com>
2021-06-30 18:26:58 -0500

    changed app test"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> still working? ... Yup!
+          Edit <code>src/App.js</code> It's free real estate!
         </p>
         <a
           className="App-link"
